12f2cd07e841a3d58d84596e1a8dcf7cf462071a
mithun_babbira <mithunbabbira@gmail.com>
2020-04-16 13:10:14 +0530

    Update weatherapp.py

--- weatherapp.py
+++ weatherapp.py
@@ -29,10 +29,6 @@ def format_response(weather):
 		final_str ='may be the u have entered the wrong details '
 
 	return final_str
-    
-
-
-
 
 root =tk.Tk()
 
@@ -43,28 +39,19 @@ background_image = tk.PhotoImage(file='landscape.png')
 background_lable = tk.Label(root, image=background_image)
 background_lable.place(relwidth=1 , relheight=1)
 
-
 frame = tk.Frame(root, bg='#9999ff' , bd=5 )
 frame.place(relx=0.5,rely=0.1, relwidth=0.75, relheight=0.08 , anchor='n')
-
-
 
 entry=tk.Entry(frame , font=('courier',14))
 entry.place(relwidth= 0.65 , relheight=1)
 
-
-
 button=tk.Button(frame, text="get weather" , font=40 , command=lambda: get_weather(entry.get()) )
 button.place(relx=0.7 , relwidth=0.3, relheight =1)
-
 
 lower_frame = tk.Frame(root, bg = '#9999ff', bd =10)
 lower_frame.place(relx=0.5 , rely = 0.25, relwidth =0.75, relheight =0.65, anchor='n')
 
-
 label=tk.Label(lower_frame,font=('courier',18))
 label.place( relwidth=1 , relheight =1)
-
-
 
 root.mainloop()
